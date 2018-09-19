656600a267cbd62e57878843f52a0656ef4b72b4
Alec <ajpershick@gmail.com>
2018-09-19 12:13:54 -0700

    finished part 1 and 2 of E2

--- exer2.hs
+++ exer2.hs
@@ -1,3 +1,18 @@
+-- Part 1: Hailstone Length
+
+hailLen n 
+  | n == 1    = 0
+  | otherwise = 1 + hailLen (hailstone n) 
+
 hailstone n
   | even n    = n `div` 2
   | otherwise = 3*n+1
+
+-- Part 2: Divisors and Primes
+divisors :: Int -> [Int]
+divisors n = [i | i <- [2..(n `div` 2)], n `mod` i == 0]
+
+primes :: Int -> [Int]
+primes n = [i | i <- [2..n], divisors i == []]
+
+-- Part 3: Joining Strings
