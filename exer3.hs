427bded229435f9b68af879f49b01a0611183f97
Alec <ajpershick@gmail.com>
2018-09-24 12:09:46 -0700

    finished fact

--- exer3.hs
+++ exer3.hs
@@ -6,12 +6,19 @@ merge (xi:xf) (yi:yf)
     | xi > yi = yi : merge (xi:xf) yf
 
 -- Tail Recursive Hailstone
-hailLen n = hailTail a n
-    where
-        hailTail a 0 = a
-        hailTail a n = hailTail 
+-- hailLen n = hailTail a n
+--     where
+--         hailTail a 1 = a
+--         hailTail a n = hailTail (3n + 1) (n `div` 2)
 
 -- stupidAdder x 0 = x
 -- stupidAdder x y = stupidAdder (x+1) (y-1)
 
 -- Factorial
+
+-- Recursively
+fact 0 = 1
+fact x = x * fact (x-1) 
+
+-- Using foldl
+fact' x = foldl (*) 1 [1..x]
