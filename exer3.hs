de209ea024a568c9cb7f41af1b8fc9e689143184
ajpershick <ajpershick@gmail.com>
2018-09-24 17:34:29 -0700

    type declarations updated

--- exer3.hs
+++ exer3.hs
@@ -2,6 +2,7 @@ import Data.Time.Calendar
 import Data.Time.Calendar.OrdinalDate
 
 -- Merge 
+merge :: [Int] -> [Int] -> [Int]
 merge x [] = x
 merge [] y = y
 merge (xi:xf) (yi:yf) 
@@ -20,19 +21,23 @@ merge (xi:xf) (yi:yf)
 -- Factorial
 
 -- Recursively
+fact :: Int -> Int
 fact 0 = 1
 fact x = x * fact (x-1) 
 
 -- Using foldl
+fact' :: Int -> Int
 fact' x = foldl (*) 1 [1..x]
 
 -- Haskell Library and Dates
 daysInYear :: Integer -> [Day]
 daysInYear y = [jan1..dec31]
   where jan1 = fromGregorian y 1 1
-        dec31 = fromGregorian y 12 365
+        dec31 = fromGregorian y 12 31
 
+isFriday :: Day -> Bool        
 isFriday day 
     | snd (mondayStartWeek day) == 5 = True
     | otherwise = False
+
 
