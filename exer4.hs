92d8e3e1eafe193dc6ad372ae160eedc502b9594
Alec <ajpershick@gmail.com>
2018-10-05 11:15:16 -0700

    finished join

--- exer4.hs
+++ exer4.hs
@@ -13,3 +13,21 @@ hailSeq n = hailTail [] n
 -- Iterative Hailstone Generator
 hailSeq' :: Int -> [Int]
 hailSeq' n = takeWhile(\x -> x /= 1) (iterate hailstone n) ++ [1]
+
+
+-- Join using fold
+join :: String -> [String] -> String
+join sep [] = ""
+join sep [x] = x
+join sep list = foldl1 (++) (map (\x -> x ++ sep) (init list)) ++ (last list)
+
+
+-- Merge Sort
+merge :: [Int] -> [Int] -> [Int]
+merge x [] = x
+merge [] y = y
+merge (xi:xf) (yi:yf) 
+    | xi <= yi = xi : merge xf (yi:yf)
+    | xi > yi = yi : merge (xi:xf) yf
+
+mergeSort list = 
