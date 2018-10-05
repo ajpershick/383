28d586df5d2fdf6a36e76ac798bef42342bba874
Alec <ajpershick@gmail.com>
2018-10-04 21:18:43 -0700

    finished hailstone part of E4

--- exer4.hs
+++ exer4.hs
@@ -1,9 +1,15 @@
-hailSeq :: Int -> [Int]
-hailSeq n = hailTail n []
-    where
-        hailTail 1 list = list
-        hailTail n list = list ++ (hailTail (hailstone n) list )
-
 hailstone n
   | even n    = n `div` 2
   | otherwise = 3*n+1
+
+-- Recursive Hailstone Generator
+hailSeq :: Int -> [Int]
+hailSeq n = hailTail [] n 
+    where
+        hailTail list 1 = list ++ [1]
+        hailTail list n = hailTail (list ++ [n]) (hailstone n)
+
+
+-- Iterative Hailstone Generator
+hailSeq' :: Int -> [Int]
+hailSeq' n = takeWhile(\x -> x /= 1) (iterate hailstone n) ++ [1]
