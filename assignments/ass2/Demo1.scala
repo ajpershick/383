7bafbafd293079279a336209a87322e88fafd84d
Alec <ajpershick@gmail.com>
2018-11-08 21:29:55 -0800

    working through scala tutorial

---
+++ assignments/ass2/Demo1.scala
@@ -0,0 +1,11 @@
+object Demo1 {
+   def main(args: Array[String]) {
+      var myVar :Int = 10;
+      val myVal :String = "Hello Scala with datatype declaration.";
+      var myVar1 = 20;
+      val myVal1 = "Hello Scala new without datatype declaration.";
+      
+      println(myVar); println(myVal); println(myVar1); 
+      println(myVal1);
+   }
+}
