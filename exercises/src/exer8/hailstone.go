0707003ef3e726af47d2940052b36a8b752fd416
Alec <ajpershick@gmail.com>
2018-11-11 14:32:11 -0800

    done Hailstone

--- exercises/src/exer8/hailstone.go
+++ exercises/src/exer8/hailstone.go
@@ -1,3 +1,10 @@
 package exer8
 
 // TODO: your Hailstone, HailstoneSequenceAppend, HailstoneSequenceAllocate functions
+func Hailstone(n uint) uint {
+	if n%2 == 0 {
+		return (n / 2)
+	} else {
+		return 3*n + 1
+	}
+}
--- exercises/src/main/main.go
+++ exercises/src/main/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"exer8"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hailstone(18) =", exer8.Hailstone(18))
+}
