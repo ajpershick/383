c23bbabead78fb9a3621baa6dd9cf09ec771edec
Alec <ajpershick@gmail.com>
2018-11-12 23:22:20 -0800

    finished e8

--- exercises/src/exer8/points.go
+++ exercises/src/exer8/points.go
@@ -1,11 +1,25 @@
 package exer8
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: The Point struct, NewPoint function, .String and .Norm methods
 type Point struct {
 	x float64
 	y float64
 }
 
-func newPoint(x float64, y float64) Point {
+// Point constructor
+func NewPoint(x float64, y float64) Point {
 	return Point{x, y}
+}
+
+func (point Point) String() string {
+	return fmt.Sprintf("(%v, %v)", point.x, point.y)
+}
+
+func (point Point) Norm() float64 {
+	return math.Sqrt(point.x*point.x + point.y*point.y)
 }
--- exercises/src/main/main.go
+++ exercises/src/main/main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
+	// My own tests
 	fmt.Println("Hailstone(18) =", exer8.Hailstone(18))
 	fmt.Println("Hailstone sequence starting at 5:", exer8.HailstoneSequenceAppend(5))
 	fmt.Println("Hailstone sequence starting at 5:", exer8.HailstoneSequenceAllocate(5))
-	fmt.Println("newPoint(5,2) = ", exer8.newPoint(5, 2))
+	fmt.Println("newPoint(5, 2) = ", exer8.NewPoint(5, 2))
+	fmt.Println("NewPoint(5, 2).String() == (5, 2) =", exer8.NewPoint(5, 2).String() == "(5, 2)")
+	fmt.Println("NewPoint(3, 4).Norm() = ", exer8.NewPoint(3, 4).Norm())
 }
